1c5b5b11c9ae63bd743fa46960cc8aeb82adab5f
Bella200625 <bbotellom@unicartagena.edu.co>
2025-07-02 02:13:05 -0500

    feat: Bella Botello Meza Taller 2 X Ejercicio 1

---
+++ Unidad.4.poo.BellaSamaiBotelloMeza/src/unidad/pkg4/poo/bellasamaibotellomeza/Taller2/Ejercicios/Producto.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.bellasamaibotellomeza.Taller2.Ejercicios;
+
+/**
+ *
+ * @author mezab
+ */
+public class Producto {
+    String nombre;
+    double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public void MD () {
+        System.out.println("El nombre del producto es: "+nombre);
+        System.out.println("El precio del producto es: "+precio);
+        
+    }
+    
+    
+}
