c0440c5be1fa3467f24c1e75ff22b7d896dfcb7c
Bella200625 <bbotellom@unicartagena.edu.co>
2025-07-02 02:32:15 -0500

    feat: Bella Botello Meza Ejercicio 3 X Taller2

---
+++ Unidad.4.poo.BellaSamaiBotelloMeza/src/unidad/pkg4/poo/bellasamaibotellomeza/Taller2/Ejercicios/Clase.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.bellasamaibotellomeza.Taller2.Ejercicios;
+
+/**
+ *
+ * @author mezab
+ */
+public class Clase {
+    String atributo1;
+    int atributo2;
+
+    public Clase(String atributo1, int atributo2) {
+        this.atributo1 = atributo1;
+        this.atributo2 = atributo2;
+    }
+    /*
+    Aqui se intenta utilizar this en un metodo static, lo cual no es valido
+    ya que this se refiere a una instancia de la case y los metodos
+    static no dependen de instancias.
+    */
+    
+    /*
+    public static void MD (){
+        System.out.println("El atributo 1 de la clase es: " +  atributo1);
+    }
+    */
+}
