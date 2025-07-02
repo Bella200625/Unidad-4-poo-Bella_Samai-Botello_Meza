a334b4e311486da804aad0a115e71d3c211f2625
Bella200625 <bbotellom@unicartagena.edu.co>
2025-07-02 00:14:45 -0500

    feat: Bella Botello Meza - ejercicio3 X taller 1

---
+++ Unidad.4.poo.BellaSamaiBotelloMeza/src/unidad/pkg4/poo/bellasamaibotellomeza/Taller1/Ejercicios/Estudiante.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.bellasamaibotellomeza.Taller1.Ejercicios;
+
+/**
+ *
+ * @author mezab
+ */
+public class Estudiante {
+    
+    String nombre;
+    int edad;
+    String curso;
+
+    public Estudiante() {
+        
+        nombre = "dc";
+        edad = 0;
+        curso = "dc";
+                
+    }
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Estudiante(String nombre, int edad, String curso) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.curso = curso;
+    }
+    
+    
+    
+    
+}
