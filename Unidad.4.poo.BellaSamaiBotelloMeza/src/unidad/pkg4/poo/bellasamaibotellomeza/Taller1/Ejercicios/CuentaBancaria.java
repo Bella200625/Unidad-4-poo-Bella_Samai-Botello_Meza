5bf7b4aeda0556fc2cb4ad98af798d60621dd490
Bella200625 <bbotellom@unicartagena.edu.co>
2025-07-02 00:04:13 -0500

    feat: Bella Botello Meza - ejercicio2 X taller 1

---
+++ Unidad.4.poo.BellaSamaiBotelloMeza/src/unidad/pkg4/poo/bellasamaibotellomeza/Taller1/Ejercicios/CuentaBancaria.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.bellasamaibotellomeza.Taller1.Ejercicios;
+
+/**
+ *
+ * @author mezab
+ */
+public class CuentaBancaria {
+    int numeroCuenta; 
+    double saldo;
+    String tipoCuenta;
+
+    // por defecto
+    public CuentaBancaria() {
+        
+     numeroCuenta = 0;
+     saldo = 0;
+     tipoCuenta = "dc"; 
+    }
+// parametrisado con dos propiedades
+    public CuentaBancaria(int numeroCuenta, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+    }
+//sobrecargado
+    public CuentaBancaria(int numeroCuenta, double saldo, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+        this.tipoCuenta = tipoCuenta;
+    }
+    
+//metodo
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria{" + "numeroCuenta=" + numeroCuenta + ", saldo=" + saldo + ", tipoCuenta=" + tipoCuenta + '}';
+    }
+    
+    
+    
+    
+
+}
