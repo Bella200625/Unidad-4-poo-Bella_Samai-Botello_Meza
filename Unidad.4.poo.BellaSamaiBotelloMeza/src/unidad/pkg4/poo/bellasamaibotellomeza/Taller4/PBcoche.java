7aed8aba058d321af68a2efd1438d85155df536b
Bella200625 <bbotellom@unicartagena.edu.co>
2025-07-02 22:24:18 -0500

    feat: Bella Botello Meza Ejercicio 2 X Taller4

---
+++ Unidad.4.poo.BellaSamaiBotelloMeza/src/unidad/pkg4/poo/bellasamaibotellomeza/Taller4/PBcoche.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package unidad.pkg4.poo.bellasamaibotellomeza.Taller4;
+import unidad.pkg4.poo.bellasamaibotellomeza.Taller4.Ejercicios.Coche;
+/**
+ *
+ * @author mezab
+ */
+public class PBcoche {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Coche coche1 = new Coche ("Suzuki", "Switft", 280.0);  
+       //No se puede acceder al atributo "modelo" porque se encuentra en private y no estamos haciendo uso de los get para acceder
+       /*
+        System.out.println(coche1.modelo);
+        */
+        
+        
+        
+        
+        
+    }
+    
+}
