52b5bacb53508f779ba81046c9c7481219f2a699
Bella200625 <bbotellom@unicartagena.edu.co>
2025-07-07 20:11:54 -0500

    Feat: Bella Botello Meza Ejercicio 3 X Taller13

---
+++ Unidad.4.poo.BellaSamaiBotelloMeza/src/unidad/pkg4/poo/bellasamaibotellomeza/Taller13/Ejercicios/Error1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.bellasamaibotellomeza.Taller13.Ejercicios;
+
+/**
+ *
+ * @author mezab
+ */
+/*
+//Para las abastractas y las interfaces es una oblicacion implementar todos sus metodos en la clase ene este caso la clase "Error1"
+//
+public class Error1 implements Nadador, Volador {
+    
+    
+}
+*/
---
+++ Unidad.4.poo.BellaSamaiBotelloMeza/src/unidad/pkg4/poo/bellasamaibotellomeza/Taller13/Ejercicios/Error2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package unidad.pkg4.poo.bellasamaibotellomeza.Taller13.Ejercicios;
+
+/**
+ *
+ * @author mezab
+ */
+public abstract class  Error2 {
+ /*  
+  public  default void HacerSonido(){
+      System.out.println("cu cu cu");
+  }
+  // Si se puede tener metodos predeterminados en clases abstractas
+  //NO se puede usar deafault en clases abstractas
+  // esto genera un erros de compilacion 
+   
+  */
+    
+}
